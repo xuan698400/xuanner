207cc5ca4b248a9e09ee7e428e51a2f671fc246e
xuan <xuan.xa@alibaba-inc.com>
2021-07-04 21:24:15 +0800

    bb

--- js/detail.js
+++ js/detail.js
@@ -1,6 +1,6 @@
 $(function () {
 
-    var load_and_refresh_article_detail = function (name) {
+    var load_and_refresh_article_detail = function () {
         //从浏览器地址参数获取文章标题
         var articleName = BW.decode(BW.decode(BW.getParamFromUrl('articleName', '')));
 
@@ -9,35 +9,28 @@ $(function () {
 
         //加载文章内容并渲染
         $.get('data/article/' + articleName + '/' + articleName + '.md', function (content) {
-            //var converter = new showdown.Converter();
-            //var html = converter.makeHtml(content);
+
+            var rendererMD = new marked.Renderer();
+            marked.setOptions({
+                renderer: rendererMD,
+                gfm: true,
+                tables: true,
+                breaks: false,
+                pedantic: false,
+                sanitize: false,
+                smartLists: true,
+                smartypants: false
+            });
+            marked.setOptions({
+                highlight: function (code) {
+                    return hljs.highlightAuto(code).value;
+                }
+            });
             $('.article-detail-content').html(marked(content));
         });
     };
 
-    var init_marked = function () {
-        // var rendererMD = new marked.Renderer();
-        // marked.setOptions({
-        //     renderer: rendererMD,
-        //     gfm: true,
-        //     tables: true,
-        //     breaks: false,
-        //     pedantic: false,
-        //     sanitize: false,
-        //     smartLists: true,
-        //     smartypants: false
-        // });
-        // var markdownString = '```js\n console.log("hello"); \n```';
-        // marked.setOptions({
-        //     highlight: function (code) {
-        //         return hljs.highlightAuto(code).value;
-        //     }
-        // });
-
-    };
-
     var init = function () {
-        init_marked();
         load_and_refresh_article_detail();
     };
     init();
