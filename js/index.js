ea3d9ff198c4e5e3408873434a27c71cebe9227a
xuan <xuan.xa@alibaba-inc.com>
2021-07-07 16:08:07 +0800

    bb

--- js/index.js
+++ js/index.js
@@ -1,29 +1,80 @@
-$(function () {
-    var currentTag = '首页';
-    var searchKey = '';
-    var allArticles = [];
+function Article() {
+    this.allArticles = [];
+    this.renderArticles = [];
+}
 
-    var render_tags = function (tagStr) {
+Article.prototype = {
+    constructor: Article,
+    init: function (callback) {
+        this.loadAll(callback);
+    },
+    loadAll: function (callback) {
+        var _this = this;
+        $.get('data/article/article.json', function (articles) {
+            _this.allArticles = articles;
+            _this.sortByCreateTime();
+            if (callback) {
+                callback();
+            }
+        });
+    },
+    getByTitle: function (title) {
+        var _this = this;
+        for (var i in _this.allArticles) {
+            var article = _this.allArticles[i];
+            if (article.title === title) {
+                return article;
+            }
+        }
+        return null;
+    },
+    sortByCreateTime: function () {
+        var _this = this;
+        _this.allArticles = _this.allArticles.sort(function (a, b) {
+            return a.createTime < b.createTime ? 1 : -1
+        });
+    },
+    filterByTag: function (tag) {
+        var _this = this;
+        this.renderArticles = [];
+        for (var i in _this.allArticles) {
+            var article = _this.allArticles[i];
+            var tagStr = article.tags;
+            if (tagStr) {
+                var tags = tagStr.split(',');
+                for (var j in tags) {
+                    if (tags[j] === tag) {
+                        _this.renderArticles.push(article);
+                    }
+                }
+            }
+        }
+    },
+    filterBySearchKey: function (searchKey) {
+        var _this = this;
+        _this.renderArticles = [];
+        for (var i in _this.allArticles) {
+            var article = _this.allArticles[i];
+            if (article['summary'].indexOf(searchKey) >= 0
+                || article['tags'].indexOf(searchKey) >= 0
+                || article['title'].indexOf(searchKey) >= 0) {
+                _this.renderArticles.push(article);
+            }
+        }
+    },
+    getTagsHtml: function (tagStr) {
         if (!tagStr) {
             return '';
         }
-
         var tags = tagStr.split(',');
-        var result = '';
+        var tagsHtml = '';
         for (var i = 0, n = tags.length; i < n; i++) {
-            result += '<p class="article-item-bottom-tag">' + tags[i] + '</p>';
+            tagsHtml += '<p class="article-item-bottom-tag">' + tags[i] + '</p>';
         }
-        return result;
-    };
-
-    var load_articles = function (callback) {
-        $.get('data/article/article.json', function (articles) {
-            callback(articles);
-        });
-    };
-
-    var refresh_articles = function (articlesUnSort) {
-        var articles = sort_articles_by_createTime(articlesUnSort);
+        return tagsHtml;
+    },
+    render: function () {
+        var _this = this;
         var articleList = $('.article-list');
         articleList.html('');
         var template = '<div class="article-item">' +
@@ -31,116 +82,82 @@ $(function () {
             '                <p class="article-item-content font-3">{summary}</p>' +
             '                <div class="article-item-bottom font-4"><p>发布时间：{createTime}</p>{tags}</div>' +
             '            </div>';
-        var content = BW.renderList(template, articles, function (k, v) {
+        var content = BW.renderList(template, this.renderArticles, function (k, v) {
             if (k === '{tags}') {
-                return render_tags(v);
+                return _this.getTagsHtml(v);
             }
             return v;
         });
         articleList.html(content);
-        init_title_click();
-    };
+    }
+};
 
-    var get_articles_by_title = function (title) {
-        for (var i in allArticles) {
-            var article = allArticles[i];
-            if (article.title === title) {
-                return article;
-            }
-        }
-        return null;
-    };
+function IndexPage() {
+    this.article = null;
+    this.currentTag = '首页';
+    this.searchKey = '';
 
-    var filter_articles_by_currentTag = function (articles) {
-        var filterArticles = [];
-        for (var i in articles) {
-            var article = articles[i];
-            var tagStr = article.tags;
-            if (tagStr) {
-                var tags = tagStr.split(',');
-                for (var j in tags) {
-                    if (tags[j] === currentTag) {
-                        filterArticles.push(article);
-                    }
-                }
-            }
-        }
-        return filterArticles;
-    };
+}
 
-    var sort_articles_by_createTime = function (articles) {
-        return articles.sort(function (a, b) {
-            return a.createTime < b.createTime ? 1 : -1
+IndexPage.prototype = {
+    constructor: IndexPage,
+    init: function () {
+        var _this = this;
+        _this.article = new Article();
+        _this.article.init(function () {
+            _this.renderByCurrentTag();
+            _this.initNav();
+            _this.initInput();
         });
-    };
-
-    var filter_articles_by_searchKey = function (articles) {
-        var filterArticles = [];
-        for (var i in articles) {
-            var article = articles[i];
-            if (article['summary'].indexOf(searchKey) >= 0
-                || article['tags'].indexOf(searchKey) >= 0
-                || article['title'].indexOf(searchKey) >= 0) {
-                filterArticles.push(article);
-            }
-        }
-        return filterArticles;
-    };
-
-    var init_nav = function () {
+    },
+    initNav: function () {
+        var _this = this;
         var allA = $('.nav').find('a');
         allA.click(function () {
             allA.removeClass('select');
             $(this).addClass('select');
-
-            //更新当前tag
-            currentTag = $(this).html();
-            //根据当前tag过了文章
-            var filterArticles = filter_articles_by_currentTag(allArticles);
-            //更新页面数据
-            refresh_articles(filterArticles);
+            _this.currentTag = $(this).html();
+            _this.renderByCurrentTag();
         });
-    };
-
-    var init_input = function () {
+    },
+    initInput: function () {
+        var _this = this;
         $(".search").on('input', function () {
-            searchKey = $(this).val();
-            var filterArticles;
-            if (searchKey && searchKey !== '') {
-                //根据关键子过滤
-                filterArticles = filter_articles_by_searchKey(allArticles);
+            _this.searchKey = $(this).val();
+            if (_this.searchKey && _this.searchKey !== '') {
+                _this.renderBySearchKey();
             } else {
-                //根据当前tag过了文章
-                filterArticles = filter_articles_by_currentTag(allArticles);
+                _this.renderByCurrentTag();
             }
-            refresh_articles(filterArticles);
         });
-    };
-
-    var init_title_click = function () {
+    },
+    initTitleClick: function () {
+        var _this = this;
         $('.article-item-title').click(function () {
             var articleName = $(this).html();
-            var article = get_articles_by_title(articleName);
+            var article = _this.article.getByTitle(articleName);
             var tags = BW.encode(BW.encode(article.tags));
             var createTime = BW.encode(BW.encode(article.createTime));
             articleName = BW.encode(BW.encode(articleName));
-
             window.open('detail.html?articleName=' + articleName + '&tags=' + tags + '&createTime=' + createTime);
         });
-    };
+    },
+    renderByCurrentTag: function () {
+        var _this = this;
+        _this.article.filterByTag(_this.currentTag);
+        _this.article.render();
+        _this.initTitleClick();
+    },
+    renderBySearchKey: function () {
+        var _this = this;
+        _this.article.filterBySearchKey(_this.searchKey);
+        _this.article.render();
+        _this.initTitleClick();
+    }
+};
 
-    var init = function () {
-        init_nav();
-        init_input();
-        //加载所有文章
-        load_articles(function (articles) {
-            allArticles = articles;
-            //根据当前tag过了文章
-            var filterArticles = filter_articles_by_currentTag(allArticles);
-            //更新页面数据
-            refresh_articles(filterArticles);
-        });
-    };
 
-    init();
+$(function () {
+    var indexPage = new IndexPage();
+    indexPage.init();
 });
