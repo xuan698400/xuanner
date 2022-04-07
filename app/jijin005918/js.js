a886203527fa62d08ccb6c8ccc984f347b380449
xuan <xuan.xa@alibaba-inc.com>
2022-04-07 22:14:37 +0800

    bb

--- app/jijin005918/js.js
+++ app/jijin005918/js.js
@@ -14,6 +14,46 @@ JiJin.prototype = {
         var _this = this;
         $.get('data.json', function (datas) {
             _this.renderDatas = datas;
+            var total = 0;
+            for (var i = 0, n = _this.renderDatas.length; i < n; i++) {
+                var data = _this.renderDatas[i];
+                var remark = '';
+                if ('' !== data.date2) {
+                    if ('做多' === data.opType) {
+                        remark = (data.amount2 - data.amount) + '元';
+                        total = total + parseInt(data.amount2 - data.amount);
+                    }
+                    if ('做空' === data.opType && '' !== data.date3) {
+                        remark = parseInt((data.num2 - data.num) * data.unitPrice3) + '元（' + (data.num2 - data.num) + '份）';
+                        total = total + parseInt((data.num2 - data.num) * data.unitPrice3);
+                    }
+                }
+                data.remark = remark;
+                if ('' !== data.remark) {
+                    data.opType += '（已完结）';
+                }
+
+                //样式
+                var sign = 'ing';
+                console.log('' !== data.remark);
+                console.log('做多' === data.opType);
+                if ('' !== data.remark && '做多（已完结）' === data.opType) {
+                    sign = 'done-in';
+                }
+                if ('' !== data.remark && '做空（已完结）' === data.opType) {
+                    sign = 'done-out';
+                }
+                data.sign = sign;
+            }
+
+            //合计
+            for (var i = 0, n = _this.renderDatas.length; i < n; i++) {
+                var data = _this.renderDatas[i];
+                if ('合计' === data.opType) {
+                    data.remark = total + "元";
+                }
+            }
+
             if (callback) {
                 callback();
             }
@@ -24,20 +64,32 @@ JiJin.prototype = {
         var contentObj = $('#dataTable');
         contentObj.html('');
         contentObj.append('<tr>\n' +
+            '        <th>类型</th>\n' +
             '        <th>日期</th>\n' +
             '        <th>净值(元/份)</th>\n' +
-            '        <th>操作类型</th>\n' +
-            '        <th>操作金额(元)</th>\n' +
-            '        <th>操作份额(份)</th>\n' +
-            '        <th>备注</th>\n' +
+            '        <th>金额(元)</th>\n' +
+            '        <th>份额(份)</th>\n' +
+            '        <th>结算日期</th>\n' +
+            '        <th>结算净值(元/份)</th>\n' +
+            '        <th>结算金额(元)</th>\n' +
+            '        <th>结算份额(份)</th>\n' +
+            '        <th>做空结算日期</th>\n' +
+            '        <th>做空结算净值(元/份)</th>\n' +
+            '        <th>结盈（单位：元）</th>\n' +
             '    </tr>');
 
         var template = '<tr class="{sign}">\n' +
+            '        <td>{opType}</td>\n' +
             '        <td>{date}</td>\n' +
             '        <td>{unitPrice}</td>\n' +
-            '        <td>{opType}</td>\n' +
             '        <td>{amount}</td>\n' +
             '        <td>{num}</td>\n' +
+            '        <td>{date2}</td>\n' +
+            '        <td>{unitPrice2}</td>\n' +
+            '        <td>{amount2}</td>\n' +
+            '        <td>{num2}</td>\n' +
+            '        <td>{date3}</td>\n' +
+            '        <td>{unitPrice3}</td>\n' +
             '        <td>{remark}</td>\n' +
             '    </tr>';
         var dataHtml = _this.renderList(template, _this.renderDatas);
